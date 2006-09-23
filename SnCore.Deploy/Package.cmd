845a8b3464b7ff51d06b5c27143bc1601953c82d
dB <dblock@dblock.org>
2006-09-23 20:07:09 +0000

    Excluding .config files from default deployment zip.

--- SnCore.Deploy/Package.cmd
+++ SnCore.Deploy/Package.cmd
@@ -8,7 +8,8 @@ echo Creating %TARGET% ...
 if EXIST "%TARGET%" ( rd /s/q "%TARGET%" )
 
 echo Copying Web
-xcopy /S /I "%~dp0..\SnCore.Web.Deploy\Release\*.*" %TARGET%
+xcopy /S /I "%~dp0..\SnCore.Web.Deploy\Release\*.*" "%TARGET%"
+del "%TARGET%\*.config"
 
 if EXIST %PROJECTNAME%.zip del %PROJECTNAME%.zip
 bin\zip32 -r %PROJECTNAME%.zip %PROJECTNAME%\*.*
