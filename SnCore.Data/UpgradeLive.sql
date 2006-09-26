07dabbd04148e423dfd775cb2e6aa381e7c869df
dB <dblock@dblock.org>
2006-09-26 22:50:16 +0000

    Missing columns from live upgrade.

--- SnCore.Data/UpgradeLive.sql
+++ SnCore.Data/UpgradeLive.sql
@@ -1,3 +1,5 @@
+ALTER TABLE dbo.AccountStory ADD [Publish] bit NOT NULL DEFAULT 1
+ALTER TABLE dbo.Account ADD [IsPasswordExpired] bit NOT NULL DEFAULT 0
 ALTER TABLE dbo.Reminder ADD [Url] nvarchar(128) NOT NULL
 ALTER TABLE dbo.Reminder DROP COLUMN Subject
 ALTER TABLE dbo.Reminder DROP COLUMN Body
