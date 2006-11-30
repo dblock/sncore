44f55f3df400422ee021715202f8afb3a018f8ac
dB <dblock@dblock.org>
2006-11-30 18:48:18 +0000

    Fixed layout on firefox.

--- SnCore.Web/ads.js
+++ SnCore.Web/ads.js
@@ -1,12 +1,13 @@
 function ads_position()
 {
- var content = document.getElementById("content")
+ var content = document.getElementById("content");
  var ads = document.getElementById("ads");
  if (ads == null || content == null) return; 
- ads.style.position = "absolute";
  ads.style.display = "";
- ads.style.top = 2;
- ads.style.left = content.offsetLeft + 835;
+ ads.style.position = "absolute";
+ ads.style.width = "115px";
+ ads.style.top = "2px";
+ ads.style.left = (content.offsetLeft + 835) + "px";
 }
 
 ads_position();
