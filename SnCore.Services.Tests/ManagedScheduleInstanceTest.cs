303a2635b8a02b678baf1a943c973f7244bfba33
dB <dblock@dblock.org>
2007-01-16 01:26:48 +0000

    Missing files, obsolete WebBackEndService.

---
+++ SnCore.Services.Tests/ManagedScheduleInstanceTest.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace SnCore.Services.Tests
+{
+    [TestFixture]
+    class ManagedScheduleInstanceTest : ManagedServiceTest
+    {
+        public ManagedScheduleInstanceTest()
+        {
+
+        }
+    }
+}
--- SnCore.Web/WebBackEndService.asmx
+++
@@ -1,1 +0,0 @@
-<%@ WebService Language="C#" CodeBehind="~/App_Code/WebBackEndService.cs" Class="SnCore.BackEndServices.WebBackEndService" %>
