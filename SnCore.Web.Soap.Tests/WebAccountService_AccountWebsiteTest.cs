7af93f170b12e2253ec64bb15ee3ef333634b176
dB <dblock@dblock.org>
2007-01-17 15:53:18 +0000

    Non-working quota test.

--- SnCore.Web.Soap.Tests/WebAccountService_AccountWebsiteTest.cs
+++ SnCore.Web.Soap.Tests/WebAccountService_AccountWebsiteTest.cs
@@ -24,5 +24,39 @@ namespace SnCore.Web.Soap.Tests.WebAccountServiceTests
             t_instance.Url = string.Format("http://www.{0}.com", Guid.NewGuid());
             return t_instance;
         }
+
+        [Test]
+        public void TestWithQuota()
+        {
+            string email = string.Format("{0}@localhost.com", Guid.NewGuid());
+            string password = Guid.NewGuid().ToString();
+            int user_id = CreateUser(email, password);
+            string ticket = Login(email, password);
+
+            const int max = 100;
+            for (int i = 0; i < max; i++)
+            {
+                WebAccountService.TransitAccountWebsite website = new WebAccountService.TransitAccountWebsite();
+                website.Name = Guid.NewGuid().ToString();
+                website.Url = string.Format("http://uri/{0}", Guid.NewGuid());
+                int id = EndPoint.CreateOrUpdateAccountWebsite(ticket, website);
+                Console.WriteLine("Created website: {0}:{1}", website.Url, id);
+            }
+
+            try
+            {
+                WebAccountService.TransitAccountWebsite website = new WebAccountService.TransitAccountWebsite();
+                website.Name = Guid.NewGuid().ToString();
+                website.Url = string.Format("http://uri/{0}", Guid.NewGuid());
+                EndPoint.CreateOrUpdateAccountWebsite(ticket, website);
+                Assert.IsTrue(false, "Missing exception at quota limit.");
+            }
+            catch (SoapException ex)
+            {
+                Console.WriteLine("Exception at quota limit: {0}", ex.Message);
+            }
+
+            DeleteUser(user_id);
+        }
     }
 }
