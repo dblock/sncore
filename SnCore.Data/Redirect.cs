7e0927385e3f4cd1e710c2d7250ac9accccefc71
dB <dblock@dblock.org>
2007-01-27 17:17:07 +0000

    Missing file.

---
+++ SnCore.Data/Redirect.cs
@@ -0,0 +1,182 @@
+﻿using System;
+    ///--------------------------------------------------------------------------------
+    ///<summary>
+    ///Persistent domain entity class representing 'Redirect' entities.
+    ///</summary>
+    ///<remarks>
+    ///
+    ///Mapping information:
+    ///This class maps to the 'Redirect' table in the data source.
+    ///</remarks>
+    ///--------------------------------------------------------------------------------
+    public class Redirect : IDbObject
+    {
+#region " Generated Code Region "
+
+        private System.Int32 m_Id;
+        private System.Int32 m_AccountId;
+        private System.DateTime m_Created;
+        private System.DateTime m_Modified;
+        private System.String m_SourceUri;
+        private System.String m_TargetUri;
+
+        ///--------------------------------------------------------------------------------
+        ///<summary>
+        ///Persistent primitive identity property.
+        ///</summary>
+        ///<remarks>
+        ///This property is an identity property.
+        ///The identity index for this property is '0'.
+        ///This property accepts values of the type 'System.Int32'.
+        ///The accessibility level for this property is 'PublicAccess'.
+        ///The accessibility level for the field 'm_Id' that holds the value for this property is 'PrivateAccess'.
+        ///
+        ///Mapping information:
+        ///The property maps to the column 'Redirect_Id' in the data source.
+        ///</remarks>
+        ///--------------------------------------------------------------------------------
+        virtual public System.Int32 Id
+        {
+            get
+            {
+                return m_Id;
+            }
+        }
+
+        ///--------------------------------------------------------------------------------
+        ///<summary>
+        ///Persistent primitive property.
+        ///</summary>
+        ///<remarks>
+        ///This property accepts values of the type 'System.Int32'.
+        ///The accessibility level for this property is 'PublicAccess'.
+        ///The accessibility level for the field 'm_AccountId' that holds the value for this property is 'PrivateAccess'.
+        ///
+        ///Mapping information:
+        ///The property maps to the column 'Account_Id' in the data source.
+        ///</remarks>
+        ///--------------------------------------------------------------------------------
+        virtual public System.Int32 AccountId
+        {
+            get
+            {
+                return m_AccountId;
+            }
+            set
+            {
+                m_AccountId = value;
+            }
+        }
+
+        ///--------------------------------------------------------------------------------
+        ///<summary>
+        ///Persistent primitive property.
+        ///</summary>
+        ///<remarks>
+        ///This property accepts values of the type 'System.DateTime'.
+        ///The accessibility level for this property is 'PublicAccess'.
+        ///The accessibility level for the field 'm_Created' that holds the value for this property is 'PrivateAccess'.
+        ///
+        ///Mapping information:
+        ///The property maps to the column 'Created' in the data source.
+        ///</remarks>
+        ///--------------------------------------------------------------------------------
+        virtual public System.DateTime Created
+        {
+            get
+            {
+                return m_Created;
+            }
+            set
+            {
+                m_Created = value;
+            }
+        }
+
+        ///--------------------------------------------------------------------------------
+        ///<summary>
+        ///Persistent primitive property.
+        ///</summary>
+        ///<remarks>
+        ///This property accepts values of the type 'System.DateTime'.
+        ///The accessibility level for this property is 'PublicAccess'.
+        ///The accessibility level for the field 'm_Modified' that holds the value for this property is 'PrivateAccess'.
+        ///
+        ///Mapping information:
+        ///The property maps to the column 'Modified' in the data source.
+        ///</remarks>
+        ///--------------------------------------------------------------------------------
+        virtual public System.DateTime Modified
+        {
+            get
+            {
+                return m_Modified;
+            }
+            set
+            {
+                m_Modified = value;
+            }
+        }
+
+        ///--------------------------------------------------------------------------------
+        ///<summary>
+        ///Persistent primitive property.
+        ///</summary>
+        ///<remarks>
+        ///This property accepts values of the type 'System.String'.
+        ///The accessibility level for this property is 'PublicAccess'.
+        ///The accessibility level for the field 'm_SourceUri' that holds the value for this property is 'PrivateAccess'.
+        ///
+        ///Mapping information:
+        ///The property maps to the column 'SourceUri' in the data source.
+        ///</remarks>
+        ///--------------------------------------------------------------------------------
+        virtual public System.String SourceUri
+        {
+            get
+            {
+                return m_SourceUri;
+            }
+            set
+            {
+                m_SourceUri = value;
+            }
+        }
+
+        ///--------------------------------------------------------------------------------
+        ///<summary>
+        ///Persistent primitive property.
+        ///</summary>
+        ///<remarks>
+        ///This property accepts values of the type 'System.String'.
+        ///The accessibility level for this property is 'PublicAccess'.
+        ///The accessibility level for the field 'm_TargetUri' that holds the value for this property is 'PrivateAccess'.
+        ///
+        ///Mapping information:
+        ///The property maps to the column 'TargetUri' in the data source.
+        ///</remarks>
+        ///--------------------------------------------------------------------------------
+        virtual public System.String TargetUri
+        {
+            get
+            {
+                return m_TargetUri;
+            }
+            set
+            {
+                m_TargetUri = value;
+            }
+        }
+
+#endregion //Generated Code Region
+
+#region " Synchronized Custom Code Region "
+#endregion //Synchronized Custom Code Region
+
+#region " Unsynchronized Custom Code Region "
+
+
+
+#endregion //Unsynchronized Custom Code Region
+
+    }
