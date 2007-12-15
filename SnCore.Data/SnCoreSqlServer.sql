e11ddaad76c6b15031573d56a0903c1d2cff1caf
dB <dblock@dblock.org>
2007-12-15 15:21:12 +0000

    Added index to AccountMessage used to select messages in a folder (default message view).

--- SnCore.Data/SnCoreSqlServer.sql
+++ SnCore.Data/SnCoreSqlServer.sql
@@ -1390,6 +1390,21 @@ CREATE TABLE [dbo].[AccountMessage](
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 END
 GO
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[AccountMessage]') AND name = N'IX_AccountMessage_Account_AccountFolder')
+CREATE NONCLUSTERED INDEX [IX_AccountMessage_Account_AccountFolder] ON [dbo].[AccountMessage] 
+(
+	[Account_Id] ASC,
+	[AccountMessageFolder_Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+GO
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[AccountMessage]') AND name = N'IX_AccountMessage_Account_AccountFolder')
+CREATE NONCLUSTERED INDEX [IX_AccountMessage_Account_AccountFolder] ON [dbo].[AccountMessage] 
+(
+	[Account_Id] ASC,
+	[AccountMessageFolder_Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+GO
 IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[AccountMessage]') AND name = N'PK_AccountMessage')
 ALTER TABLE [dbo].[AccountMessage] ADD  CONSTRAINT [PK_AccountMessage] PRIMARY KEY CLUSTERED 
 (
