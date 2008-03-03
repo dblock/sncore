8dd9c05626ea24c5018deda098e657b418f997f4
dB <dblock@dblock.org>
2008-03-03 13:54:18 +0000

    Added volumes of documentation and building/running unit tests.

--- build.cmd
+++ build.cmd
@@ -9,7 +9,7 @@ if "%BUILD_CONFIGURATION%"=="" set BUILD_CONFIGURATION=Debug
 if "%FrameworkVersion%"=="" set FrameworkVersion=v2.0.50727
 if "%FrameworkDir%"=="" set FrameworkDir=%SystemRoot%\Microsoft.NET\Framework
 echo Framework: %FrameworkDir%
-PATH=%PATH%;%FrameworkDir%\%FrameworkVersion%;%ProgramFiles%\Microsoft Visual Studio 8\VC\BIN;%ProgramFiles%\Microsoft Visual Studio 8\Common7\Tools
+PATH=%PATH%;%FrameworkDir%\%FrameworkVersion%;%ProgramFiles%\Microsoft Visual Studio 8\VC\BIN;%ProgramFiles%\Microsoft Visual Studio 8\Common7\Tools;%ProgramFiles%\NUnit 2.4.1\bin
 echo %PATH%
 msbuild.exe SnCore.proj /t:%BUILD_TARGET% /p:"Configuration=%BUILD_CONFIGURATION%"
 endlocal
